d54dd7427d196453ad568eead0110e952a604a2c
PetroHrynyk <grynyk82@gmail.com>
2022-08-04 16:13:53 +0300

    task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,23 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
 
+const galleryMarkup = galleryItems
+  .map(
+    item =>
+      `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" 
+    alt="${item.description}"/>
+    </a>`
+  )
+  .join('');
+
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+let modal = new SimpleLightbox('.gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 console.log(galleryItems);
