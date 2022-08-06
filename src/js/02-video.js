86765ffb3d7311be8e5c1a494204feb08ba0fac9
PetroHrynyk <grynyk82@gmail.com>
2022-08-06 13:08:04 +0300

    task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
-
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('#vimeo-player');
+// console.log(iframe);
+const player = new Player(iframe);
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+    // console.log(data.percent);
+  }, 1000)
+);
+const currentTime = localStorage.getItem('videoplayer-current-time');
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
